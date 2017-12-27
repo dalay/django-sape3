c451f9486e187c0818957d04f6277939663dbdd4
dalay <dalay@drupalka.ru>
2017-12-28 00:23:36 +0300

    changes in templates

--- sape/templatetags/sape.py
+++ sape/templatetags/sape.py
@@ -1,6 +1,5 @@
 from django import template
 from django.conf import settings
-from django.utils.safestring import mark_safe
 
 from sape.sape_client import sape_manager
 
@@ -12,7 +11,7 @@ SAPE_DIR = getattr(settings, 'SAPE_DIR', '.')
 
 
 @register.inclusion_tag('sape/links.html', takes_context=True)
-def sape_links(context, title=True):
+def sape_links(context, title=None):
     request = context['request']
     if request:
         query_string = request.META.get('QUERY_STRING', '')
@@ -20,7 +19,6 @@ def sape_links(context, title=True):
         uri = ''.join([request.META["PATH_INFO"],
                         len(query_string) and '?' or '',
                         query_string])
-
         links = sape_manager.get_links(uri)
         if links:
             return {'links': links, 'block_title': title}
